dafab66b9f8974821dc2de854e7f762179509104
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-06-06 18:41:39 +0530

    Internship task 3

---
+++ Level 2/Task 3/src/App.jsx
@@ -0,0 +1,17 @@
+import { Cards } from "./components/Cards"
+import { Appbar } from "./components/Appbar"
+import { Table } from "./components/Table"
+import { Footer } from "./components/footer"
+
+function App() {
+  return (
+    <main>
+      <Appbar />
+      <Cards />
+      <Table />
+      <Footer />
+    </main>
+  )
+}
+
+export default App
